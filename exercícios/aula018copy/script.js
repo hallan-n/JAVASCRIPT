c5a04295b341ba62528eef873e9933872432a066
hallan-5 <104338375+hallan-n@users.noreply.github.com>
2022-05-06 12:51:29 -0300

    Ajustes finais com resposinvo

--- exercícios/aula018copy/script.js
+++ exercícios/aula018copy/script.js
@@ -4,17 +4,18 @@ function mudarsol(){
     var img = window.document.getElementById('imagem')
     var dia = new Date()
     var hora = dia.getHours()
+    var min = dia.getMinutes()
     if(hora > 0 && hora < 12){
-        msg.innerHTML = `Bom dia! Agora são ${hora}h`
+        msg.innerHTML = `Bom dia! Agora são ${hora}h:${min}`
         img.src = 'manha.png'
         body.style.background = '#ffe952'
         
     }else if(hora >= 12 && hora < 18){
-        msg.innerHTML = `Boa tarde! agora são ${hora}h`
+        msg.innerHTML = `Boa tarde! agora são ${hora}h:${min}`
         img.src = 'tarde.png'
         body.style.background = '#f5a671'
     }else{
-        msg.innerHTML = `Boa noite! agora são ${hora}h`
+        msg.innerHTML = `Boa noite! agora são ${hora}h:${min}`
         img.src = 'noite.png'
         body.style.background = '#7e7e7e'
     }
